4b8126b28147edbd5f76e8dad1ec22336a984586
aolssonify <andreolssonn@gmail.com>
2024-10-24 17:12:25 +0200

    sql.php

--- sql.php
+++ sql.php
@@ -3,7 +3,7 @@ header('Content-type: text/html; charset=utf-8');
 
 try {
     // Använd tjänstnamnet 'db' som host eftersom det är vad MySQL-tjänsten heter i Docker Compose
-    $dsn = 'mysql:host=db;dbname=mydatabase;charset=utf8mb4';
+    $dsn = 'mysql:host=db;dbname=kirunabio;charset=utf8mb4';
     $pdo = new PDO($dsn, 'user', 'password', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
